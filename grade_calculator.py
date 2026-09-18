96d106e716e8af5b5b1df127c98c4fd98db40f75
sunyeong Kim <babymouse333@gmail.com>
2026-09-18 10:59:07 -0400

    Add grade calculator program

---
+++ grade_calculator.py
@@ -0,0 +1,18 @@
+MIN_SCORE = 0.00; MAX_SCORE = 100.00
+user_grade = input("Enter a grade: ")
+try:
+    grade = float(user_grade)
+except ValueError:
+    print(f"{user_grade} is not a number. Please enter a number")
+    raise SystemExit(1)
+if grade < MIN_SCORE or grade > MAX_SCORE:
+    print(f"{grade} is outside the range.(MIN : {MIN_SCORE}, MAX : {MAX_SCORE})")       
+    print("Please enter a valid grade.")
+    raise SystemExit(1)
+if grade >= 90.00:
+   result = "A"
+elif 60.00 <= grade < 90.00:
+    result = "B"
+else:
+    result = "C"
+print(f"{grade} is {result}")
